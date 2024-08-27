930fa7b59e2f2f5e4f800601b9fa2a64c8273319
Prateek <bhandariprateek64@gmail.com>
2024-08-27 12:17:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const typed=new Typed('.text', {
+            strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+            typeSpeed: 50,
+            backSpeed: 25,
+            loop: true
+        });
+});
